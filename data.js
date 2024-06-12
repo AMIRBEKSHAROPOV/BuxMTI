822c35b1675722a71961c575d1ae5a729be3ab9f
AMIRBEKSHAROPOV <sharopovg@gmail.com>
2024-06-12 22:58:42 +0300

    Load Marzipano

---
+++ data.js
@@ -0,0 +1,574 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-institut-kirish-qismi",
+      "name": "1. Institut Kirish qismi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.10908532969719964,
+        "pitch": -0.13107518981827404,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.134409634535281,
+          "pitch": 0.31101187590340373,
+          "rotation": 0,
+          "target": "1-2-institutning-3-ta-bino-korinishi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6405850912531879,
+          "pitch": -0.049279165882088805,
+          "title": "BUXORO MUHANDISLIK TEXNOLOGIYA INSTITUTI",
+          "text": "BMTI-Buxoro Muhandislik Instituti 46-yoshda!!!<div><span lang=\"EN-US\" style=\"font-size: 9pt; line-height: 107%; font-family: &quot;Open Sans&quot;, sans-serif;\">Agar\ntariximizga nazar tashlar ekanmiz, yurtimizda bo‘lgan ikki Renessans davrida\nham ilm nihoyatda qadrlangan edi. IX-XII asrlarda mintaqamizda yuz bergan\nbirinchi Sharq Uyg‘onish davrida yuzlab buyuk allomalar, qomusiy bilim\nsohiblari, mashhur mutafakkirlar yetishib chiqqan. Ularning aqlu zakosi\numumbashariyat taraqqiyotiga mislsiz hissa qo‘shgan.&nbsp;</span><div><div><br></div></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-institutning-3-ta-bino-korinishi",
+      "name": "2. Institutning 3 ta bino ko'rinishi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.9324255818175295,
+        "pitch": -0.2484390945692727,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.897946900600738,
+          "pitch": 0.4062758071422934,
+          "rotation": 3.141592653589793,
+          "target": "0-1-institut-kirish-qismi"
+        },
+        {
+          "yaw": 2.6838865234121414,
+          "pitch": 0.2093213993169254,
+          "rotation": 5.497787143782138,
+          "target": "2-3-bog-besetkalar"
+        },
+        {
+          "yaw": 2.5291595055014,
+          "pitch": 0.2814106355690349,
+          "rotation": 4.71238898038469,
+          "target": "3-3-parkovka"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0837408264203017,
+          "pitch": -0.11282920790736739,
+          "title": "1-BINO",
+          "text": "1-binoda 3 ta fakultet bor.&nbsp;<div>1) Paxtava yengil sanoat texnologiyalari fakulteti.</div><div>2) To'qimachilik va charm sanoati fakulteti.</div><div>3) Yer usti transporti tizimlari fakulteti</div>"
+        },
+        {
+          "yaw": -2.3534340103334266,
+          "pitch": -0.11238993378357165,
+          "title": "3-BINO",
+          "text": "3-binoda 2 ta fakultet bor.&nbsp;<div>1) Arxitektura qurilish fakulteti.</div><div>2) Neft-gaz texnologiyasi fakulteti.</div><div><br></div>"
+        },
+        {
+          "yaw": 2.397716331050937,
+          "pitch": -0.13580486607018827,
+          "title": "2-BINO",
+          "text": "2-binoda 3 ta fakultet bor.&nbsp;<div>1) Arxitektura-qurilish fakulteti.</div><div>2) Energetika fakulteti.</div><div>3) Texnologik jarayonlarni boshqaruv tizimlari fakulteti.</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-bog-besetkalar",
+      "name": "3. Bo'g besetkalar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.741756966852737,
+        "pitch": -0.2022220861367252,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1192454925758906,
+          "pitch": 0.350541948498833,
+          "rotation": 2.356194490192345,
+          "target": "1-2-institutning-3-ta-bino-korinishi"
+        },
+        {
+          "yaw": 2.594553878160802,
+          "pitch": 0.24168888877480654,
+          "rotation": 3.9269908169872414,
+          "target": "3-3-parkovka"
+        },
+        {
+          "yaw": -2.9207885357274836,
+          "pitch": 0.0961868758905684,
+          "rotation": 6.283185307179586,
+          "target": "5-5-ikkinchi-bino-old-tomoni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.066913044155454,
+          "pitch": 0.1935181221484079,
+          "title": "Talabalar uchun besetkalar",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-3-parkovka",
+      "name": "3. Parkovka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.806769541993301,
+        "pitch": -0.029314008221025034,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8251427298861049,
+          "pitch": 0.21348111672521597,
+          "rotation": 7.0685834705770345,
+          "target": "2-3-bog-besetkalar"
+        },
+        {
+          "yaw": 0.3978886482578705,
+          "pitch": 0.5527136302818931,
+          "rotation": 9.42477796076938,
+          "target": "1-2-institutning-3-ta-bino-korinishi"
+        },
+        {
+          "yaw": -0.024482599218174528,
+          "pitch": 0.10435725380117944,
+          "rotation": 0.7853981633974483,
+          "target": "4-4-ikkinchi-bino"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.17392646811472012,
+          "pitch": 0.2906066810813641,
+          "title": "P-parking",
+          "text": "Mashinalar parkovkasi uchun institut ichida joy"
+        }
+      ]
+    },
+    {
+      "id": "4-4-ikkinchi-bino",
+      "name": "4. Ikkinchi bino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.712718061247056,
+        "pitch": -0.3786392728551604,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.140823747048728,
+          "pitch": 0.08640642590625802,
+          "rotation": 7.0685834705770345,
+          "target": "5-5-ikkinchi-bino-old-tomoni"
+        },
+        {
+          "yaw": 2.7151660824059665,
+          "pitch": 0.25992026809721835,
+          "rotation": 3.141592653589793,
+          "target": "3-3-parkovka"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.151057252116905,
+          "pitch": -0.5886020688628673,
+          "title": "2-BINO",
+          "text": "<b><span style=\"font-size: 11pt; line-height: 107%; font-family: &quot;Open Sans&quot;, sans-serif;\">Buxoro\nmuhandislik-texnologiya instituti rektorining 2019-yil 25-iyul №160-U sonli\nbuyrug‘i bilan Oliy va o‘rta maxsus ta’lim vazirligi va institut Kengashining\n2019-yil 1-apreldagi&nbsp;296 son&nbsp;buyrug‘i bilan qo‘shimcha va\no‘zgartirishlar kiritilgan “Oliy ta‘lim muassasining fakulteti to‘g‘risida”gi\nnamunaviy nizomga muvofiq ishlab chiqilgan bo‘lib “</span></b><strong><span style=\"font-size: 11pt; line-height: 107%; font-family: &quot;Open Sans&quot;, sans-serif; border: 1pt none windowtext; padding: 0cm; font-weight: normal;\">Texnologik jarayonlarni\nboshqaruv tizimlari</span></strong><b><span style=\"font-size: 11pt; line-height: 107%; font-family: &quot;Open Sans&quot;, sans-serif;\">”\nfakulteti faoliyatini tashkil etish va yuritish tartibini belgilaydi</span></b>"
+        }
+      ]
+    },
+    {
+      "id": "5-5-ikkinchi-bino-old-tomoni",
+      "name": "5. Ikkinchi bino old tomoni",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.046304653624339664,
+        "pitch": -0.31023992033938796,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4062518036982681,
+          "pitch": 0.0885469519296116,
+          "rotation": 4.71238898038469,
+          "target": "4-4-ikkinchi-bino"
+        },
+        {
+          "yaw": -0.7351057152749014,
+          "pitch": 0.22290435728137936,
+          "rotation": 3.9269908169872414,
+          "target": "2-3-bog-besetkalar"
+        },
+        {
+          "yaw": 0.09544346119146851,
+          "pitch": 0.08080698211758985,
+          "rotation": 0,
+          "target": "6-6-2-chi-bino-kirish-qismi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6142467070617812,
+          "pitch": -0.40431262680880486,
+          "title": "2-BINO",
+          "text": "2-bino.&nbsp;<div>3 ta fakultet</div><div>16 ta yo'nalishni o'z ichida oladi</div><div>5-etajdan iborat</div>"
+        }
+      ]
+    },
+    {
+      "id": "6-6-2-chi-bino-kirish-qismi",
+      "name": "6. 2-chi bino kirish qismi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.7533410955036821,
+        "pitch": 0.40306761303935446,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1185144218836474,
+          "pitch": 1.0192172017452972,
+          "rotation": 3.141592653589793,
+          "target": "5-5-ikkinchi-bino-old-tomoni"
+        },
+        {
+          "yaw": -0.33990777819989404,
+          "pitch": 0.39560986881270743,
+          "rotation": 12.566370614359176,
+          "target": "7-7-2-chi-bino-2-etaj"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.328982707235605,
+          "pitch": 0.15387049352431248,
+          "title": "2-BINO&nbsp;",
+          "text": "<b><span style=\"font-size:11.0pt;line-height:107%;\nfont-family:&quot;Open Sans&quot;,sans-serif;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;color:#F2F2F2;mso-themecolor:background1;mso-themeshade:242;\nmso-style-textfill-fill-color:#F2F2F2;mso-style-textfill-fill-themecolor:background1;\nmso-style-textfill-fill-alpha:100.0%;mso-style-textfill-fill-colortransforms:\nlumm=95000;mso-ansi-language:RU;mso-fareast-language:EN-US;mso-bidi-language:\nAR-SA\">Buxoro muhandislik-texnologiya instituti “<a href=\"https://www.timeshighereducation.com/world-university-rankings/bukhara-engineering-technological-institute\" style=\"transition: all 0.2s ease 0s;\"><span style=\"color:#F2F2F2;mso-themecolor:background1;mso-themeshade:242;mso-style-textfill-fill-color:\n#F2F2F2;mso-style-textfill-fill-themecolor:background1;mso-style-textfill-fill-alpha:\n100.0%;mso-style-textfill-fill-colortransforms:lumm=95000;border:none windowtext 1.0pt;\nmso-border-alt:none windowtext 0cm;padding:0cm\">reporter</span></a>” maqomida\nqayd etilib, TOP-1 000 talik reytingdan joy olish uchun davogar bo’lmoqda.Ushbu\nreytingda universitetlarning 18 ta indikator asosida 5 ta yo‘nalishdagi\nsamaradorlik ko‘rsatkichlari baholandi: o‘qitish, tadqiqot muhiti, tadqiqot\nsifati, sanoat va xalqaro istiqbollar</span></b>"
+        },
+        {
+          "yaw": 0.2153939806767582,
+          "pitch": 0.2986474437495037,
+          "title": "Lift",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "7-7-2-chi-bino-2-etaj",
+      "name": "7. 2-chi bino 2-etaj",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.23722912251030692,
+        "pitch": 0.35780561465725924,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.000500388029406551,
+          "pitch": 0.6024164717749798,
+          "rotation": 0,
+          "target": "8-8-koridor-qismi-xonalar"
+        },
+        {
+          "yaw": -0.3955604891780027,
+          "pitch": 0.8184745013952242,
+          "rotation": 4.71238898038469,
+          "target": "6-6-2-chi-bino-kirish-qismi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5005722983858796,
+          "pitch": 0.37199362520851587,
+          "title": "2-etaj",
+          "text": "2 etajning o'zida 32ta xona mavjud&nbsp;<div><br></div>"
+        },
+        {
+          "yaw": -1.702588414471876,
+          "pitch": 0.6101536745741623,
+          "title": "Kirish/Chiqish",
+          "text": "Zina orqali kirish va chiqish"
+        }
+      ]
+    },
+    {
+      "id": "8-8-koridor-qismi-xonalar",
+      "name": "8. Koridor qismi (xonalar)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.08547237424104992,
+        "pitch": 0.2331110774799825,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7146078791309538,
+          "pitch": 0.6612642268647573,
+          "rotation": 4.71238898038469,
+          "target": "9-9-kompyuter-xona"
+        },
+        {
+          "yaw": 0.03063097917728541,
+          "pitch": 0.8613644102755291,
+          "rotation": 3.141592653589793,
+          "target": "7-7-2-chi-bino-2-etaj"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7707461337684727,
+          "pitch": 0.12270188661908144,
+          "title": "Kompyuter xona",
+          "text": "209- xona o'z ichiga oladi<div>1) Kompyuter</div><div>2) Kompyuetr stoli</div><div>3) Ikki kishilik parta</div><div>4) Televizor</div><div>5) Stul (oddiy va pismenniy)</div><div>6) Doska&nbsp;</div><div>7) Tribuna stoli<br><div><br></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "9-9-kompyuter-xona",
+      "name": "9. Kompyuter xona",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.4020938413460158,
+        "pitch": -0.010637024067957412,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.515385568217054,
+          "pitch": 0.3383701599888873,
+          "rotation": 0,
+          "target": "8-8-koridor-qismi-xonalar"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
